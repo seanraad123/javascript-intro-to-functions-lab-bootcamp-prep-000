ae808ba7d03789393996d08db928649005c18a88
Sean Conrad <seanconrad123@gmail.com>
2018-03-29 22:01:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  }
+  
+  else if (string === string.toUpperCase()){
     
   }
+  
 }
